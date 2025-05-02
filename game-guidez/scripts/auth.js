8ebc5c6acec644894a16f3214bc198f02b25a659
Vinicius Nascimento Silva <a62860@alunos.ipb.pt>
2025-05-02 19:36:04 +0100

    create form

--- game-guidez/scripts/auth.js
+++ game-guidez/scripts/auth.js
@@ -12,6 +12,23 @@ auth.onAuthStateChanged((user) => {
     }
 });
 
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() => {
+        // close the modal and reset form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch(err => {
+        console.log(err.message);
+    });
+});
+
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
