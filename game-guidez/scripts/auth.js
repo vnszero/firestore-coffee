a75d2307326274230034bc398cfb0a320fd4e463
Vinicius Nascimento Silva <a62860@alunos.ipb.pt>
2025-05-02 17:40:54 +0100

    login and logout

--- game-guidez/scripts/auth.js
+++ game-guidez/scripts/auth.js
@@ -1,3 +1,12 @@
+// listen for auth status changes
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        console.log('user logged in: ', user);
+    } else {
+        console.log('user logged out');
+    }
+});
+
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -8,9 +17,31 @@ signupForm.addEventListener('submit', (e) => {
 
     // signup user
     auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-        console.log(cred.user);
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
+    });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
     });
 })
